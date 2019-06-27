9aa60d17ad81ac9727a6d1fcb95a0403987ed2c8
JohnTheBeloved <topzy20@gmail.com>
2019-06-27 14:01:09 +0100

    Addd benchmark for DynamicForm7 500_1X50000

---
+++ src/jmh/java/org/javarosa/benchmarks/dynamicforms/DynamicXForm7_500_1X50000_Benchmark.java
@@ -0,0 +1,51 @@
+package org.javarosa.benchmarks.dynamicforms;
+
+import org.javarosa.benchmarks.utils.builder.XFormFileGenerator;
+import org.javarosa.core.model.FormDef;
+import org.javarosa.xform.parse.FormParserHelper;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
+import static org.javarosa.benchmarks.utils.BenchmarkUtils.dryRun;
+import static org.javarosa.benchmarks.utils.BenchmarkUtils.getWorkingDir;
+
+public class DynamicXForm7_500_1X50000_Benchmark {
+
+    public static void main(String[] args) {
+        dryRun(DynamicXForm7_500_1X50000_Benchmark.class);
+    }
+
+    @State(Scope.Thread)
+    public static class FormTypesState {
+        Path formPath ;
+
+        @Setup(Level.Trial)
+        public void initialize() throws IOException {
+            XFormFileGenerator xFormFileGenerator = new XFormFileGenerator();
+            String title = "DynamicXForm7_500_1X50000";
+            final int noOfQuestions = 500;
+            final int noOfInternalSecondaryInstances = 1;
+            final int noOf2ndryInstanceElements = 50000;
+            final int noOfQuestionGroups = 1;
+            final int noOfExternalSecondaryInstances = 0;
+            final Path WORKING_DIR = getWorkingDir();
+            File xFormXmlFile = xFormFileGenerator.generate(title, noOfQuestions, noOfQuestionGroups, noOfInternalSecondaryInstances, noOfExternalSecondaryInstances, noOf2ndryInstanceElements, WORKING_DIR);
+            formPath = xFormXmlFile.toPath();
+        }
+    }
+
+    @Benchmark
+    public void benchmarkDynamicXForm(FormTypesState state, Blackhole bh) throws IOException {
+        FormDef formDef =  FormParserHelper.parse(state.formPath);
+        bh.consume(formDef);
+    }
+
+}
